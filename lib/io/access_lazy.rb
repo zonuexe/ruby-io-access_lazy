b3f672d505caa2f954b42004b3b9ae796328ea3e
USAMI Kenta <tadsan@zonu.me>
2013-03-09 23:22:25 +0900

    init repo.

---
+++ lib/io/access_lazy.rb
@@ -0,0 +1,7 @@
+require "io/access_lazy/version"
+
+class IO
+  class AccessLazy
+    # Your code goes here...
+  end
+end
---
+++ lib/io/access_lazy/version.rb
@@ -0,0 +1,5 @@
+class IO
+  class AccessLazy
+    VERSION = "0.0.1"
+  end
+end
